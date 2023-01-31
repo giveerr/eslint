34113f2c2cc3b975e20a611a7d42afe6513b0865
giveerr <givessomeerr.e@gmail.com>
2023-01-31 18:59:22 +0300

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e "module.exports = require('./dist/index.js').default;\n" > index.js
+echo "module.exports = require('./dist/index.js').default;" > index.js
 echo "Created index.js"
 
 tsup src/index.ts --dts $1
